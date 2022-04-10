e02c28bee1e8d2ee7c38d6aeb4bb2743d0f39be8
MyShine8 <skybskater25@gmail.com>
2022-04-10 17:14:10 -0500

    added files

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hey Mickie!");
+using System;
+namespace MyNewApp
+{   
+    class Program
+    }
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Hello World!");
+        } 
+    }
+}
